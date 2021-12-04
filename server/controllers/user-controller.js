a418d5a0c176bb69545c5264e4cfda955cfdce36
Max <zeetreex@gmail.com>
2021-12-04 18:41:16 +0200

    refresh refreshToken functionality

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -31,7 +31,10 @@ class UserController {
 
     async logout(req, res, next){
         try {
-
+            const {refreshToken} = req.cookies;
+            const token = await userService.logout(refreshToken)
+            res.clearCookie('refreshToken');
+            return res.json(token)
         } catch (e) {
             next(e)
         }
@@ -49,7 +52,10 @@ class UserController {
 
     async refresh(req, res, next){
         try {
-
+            const {refreshToken} = req.cookies;
+            const userData = await userService.refresh(refreshToken);
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30*24*60*60*1000, httpOnly: true})
+            return res.json(userData)
         } catch (e) {
             next(e)
         }
